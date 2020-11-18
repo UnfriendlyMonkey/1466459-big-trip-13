1373b0dcc88ecaa981bf82b7980d35a8be9cbd28
Andrey Sereda <andreysereda@ya.ru>
2020-11-19 00:16:50 +0300

    добавлены модули с меню и список мест назначения

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import {createTripInfoTemplate} from "./view/trip-info.js";
+import {createTripTabsTemplate} from "./view/trip-tabs.js";
+import {createTripFiltersTemplate} from "./view/trip-filters.js";
+import {createListSortTemplate} from "./view/list-sort.js";
+import {createListTemplate} from "./view/list.js";
+import {createListItemTemplate} from "./view/list-item.js";
+
+const INITIAL_POINTS_NO = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const tripMainElement = document.querySelector(`.trip-main`);
+const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
+
+const tripEventsElement = document.querySelector(`.trip-events`);
 
 render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
+render(tripControlsElement, createTripTabsTemplate(), `afterbegin`);
+render(tripControlsElement, createTripFiltersTemplate(), `beforeend`);
+render(tripEventsElement, createListSortTemplate(), `beforeend`);
+render(tripEventsElement, createListTemplate(), `beforeend`);
+
+const tripListElement = tripEventsElement.querySelector(`.trip-events__list`);
+
+for (let i = 0; i < INITIAL_POINTS_NO; i++) {
+  render(tripListElement, createListItemTemplate(), `beforeend`);
+}
+
---
+++ src/view/list.js
@@ -0,0 +1,3 @@
+export const createListTemplate = () => {
+  return `<ul class="trip-events__list"></ul>`;
+};
