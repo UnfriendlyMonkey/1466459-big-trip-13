e18b44cce7dd0da6347bd4e5be984ee31827e12d
Andrey Sereda <andreysereda@ya.ru>
2020-11-19 00:41:47 +0300

    добавил формы создания и редактирования точки

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import {createTripFiltersTemplate} from "./view/trip-filters.js";
 import {createListSortTemplate} from "./view/list-sort.js";
 import {createListTemplate} from "./view/list.js";
 import {createListItemTemplate} from "./view/list-item.js";
+import {createAddPointTemplate} from "./view/add-point.js";
+import {createEditPointTemplate} from "./view/edit-point.js";
 
 const INITIAL_POINTS_NO = 3;
 
@@ -24,7 +26,10 @@ render(tripEventsElement, createListTemplate(), `beforeend`);
 
 const tripListElement = tripEventsElement.querySelector(`.trip-events__list`);
 
+render(tripListElement, createAddPointTemplate(), `afterbegin`);
+
+render(tripListElement, createEditPointTemplate(), `beforeend`);
+
 for (let i = 0; i < INITIAL_POINTS_NO; i++) {
   render(tripListElement, createListItemTemplate(), `beforeend`);
 }
-
