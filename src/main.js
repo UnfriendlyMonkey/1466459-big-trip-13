b92dbfaf347b9ee481616dec9b03fd6646bc94db
Andrey Sereda <andreysereda@ya.ru>
2020-11-19 23:17:54 +0300

    Правки по замечаниям наставника

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import {createTripInfoTemplate} from "./view/trip-info.js";
 import {createTripTabsTemplate} from "./view/trip-tabs.js";
 import {createTripFiltersTemplate} from "./view/trip-filters.js";
 import {createListSortTemplate} from "./view/list-sort.js";
-import {createListTemplate} from "./view/list.js";
-import {createListItemTemplate} from "./view/list-item.js";
-import {createAddPointTemplate} from "./view/add-point.js";
+import {createTripEventsListTemplate} from "./view/trip-event-list.js";
+import {createTripEventItemTemplate} from "./view/trip-event-item.js";
+import {createAddPointFormTemplate} from "./view/add-point.js";
 import {createEditPointTemplate} from "./view/edit-point.js";
 
 const INITIAL_POINTS_NO = 3;
@@ -22,14 +22,14 @@ render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
 render(tripControlsElement, createTripTabsTemplate(), `afterbegin`);
 render(tripControlsElement, createTripFiltersTemplate(), `beforeend`);
 render(tripEventsElement, createListSortTemplate(), `beforeend`);
-render(tripEventsElement, createListTemplate(), `beforeend`);
+render(tripEventsElement, createTripEventsListTemplate(), `beforeend`);
 
 const tripListElement = tripEventsElement.querySelector(`.trip-events__list`);
 
-render(tripListElement, createAddPointTemplate(), `afterbegin`);
+render(tripListElement, createAddPointFormTemplate(), `afterbegin`);
 
 render(tripListElement, createEditPointTemplate(), `beforeend`);
 
 for (let i = 0; i < INITIAL_POINTS_NO; i++) {
-  render(tripListElement, createListItemTemplate(), `beforeend`);
+  render(tripListElement, createTripEventItemTemplate(), `beforeend`);
 }
--- src/view/list.js
+++
@@ -1,3 +0,0 @@
-export const createListTemplate = () => {
-  return `<ul class="trip-events__list"></ul>`;
-};
